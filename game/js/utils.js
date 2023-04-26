02ae97ebf421fb2c1a04534c3d2826f95512d4c7
unknown <elachraf6@gmail.com>
2023-04-26 16:07:48 +0100

    a

--- game/js/utils.js
+++
@@ -1,8 +0,0 @@
-function collision(object1, object2) {
-    return (
-        object1.position.y + object1.height >= object2.positionY &&
-        object1.position.y <= object2.positionY + object2.height &&
-        object1.position.x + object1.width >= object2.positionX &&
-        object1.position.x <= object2.positionX + object2.width
-    )
-}
